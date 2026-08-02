ebfa8adff7d5a2aca32da2b0edf1b0ff17a86891
kushal rathore <singhkushal235@example.com>
2026-08-02 15:10:09 +0530

    feat: setup application routing and layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import { ThemeToggle } from "@/components/ui/theme-toggle";
-import { Button } from "@/components/ui/button";
+import AppRouter from "@/routes/AppRouter";
 
 function App() {
-  return (
-    <div className="flex h-screen items-center justify-center bg-background text-foreground">
-      <Button>hii</Button>
-      <ThemeToggle />
-    </div>
-  );
+  return <AppRouter></AppRouter>;
 }
 
 export default App;
---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Routes, Route,Navigate } from "react-router-dom";
+
+import AppLayout from "@/layouts/AppLayout";
+import AuthLayout from "@/layouts/AuthLayout";
+import ProtectedRoute from "@/routes/ProtectedRoute";
+
+import Login from "@/pages/auth/Login";
+import Dashboard from "@/pages/dashboard/Dashboard";
+import NotFound from "@/pages/NotFound";
+
+export default function AppRouter() {
+  return (
+    <Router>
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+        <Route element={<ProtectedRoute />}>
+          <Route element={<AppLayout />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+}
